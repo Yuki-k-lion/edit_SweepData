fab723a75f1c72d2a7cc47ec67f03c5eb8e41d23
yuki komatsu <yuki.k.lion@gmail.com>
2019-01-26 15:41:14 +0900

    fix:Cだと配列のコピーは使用できないので、

--- editSweepToSingle/SweepToSingleCSV.c
+++ editSweepToSingle/SweepToSingleCSV.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define N 256 // 1行の最大文字数(バイト数)
 #define COL 3 //ファイルデータの列数
 #define ROW 1001 //ファイルデータの行数
@@ -8,9 +9,9 @@ int main(void) {
   //宣言
 	FILE *fp; // FILE型構造体
 	char file_name[] = "../data/sweep_saf_w_edit.txt";
-  char new_filename[N],single_data[ROW][COL];
+  char new_filename[N],single_data[ROW][COL][N];
   // float single_data[ROW][COL];
-  // char str[N],str1[N],str2[N],str3[N];
+  char str[N],str1[N],str2[N],str3[N];
   // float f1,f2,f3;
 
 	fp = fopen(file_name, "r"); // ファイルを開く。失敗するとNULLを返す。
@@ -24,13 +25,24 @@ int main(void) {
 	}
 
   //ファイルの中身を取り出して表示
- int datacount = 0;
-  while (fscanf(fp, "\t%s\t%s\t%s", &single_data[datacount][0], &single_data[datacount][1], &single_data[datacount][2]) !=EOF) {
-    datacount += 1;
-    if (datacount <= 10) {
-      printf("%s,%s,%s\n",single_data[datacount][0], single_data[datacount][1], single_data[datacount][2]);
-    }
-  }
+  int datacount = 0;
+   while (fscanf(fp, "\t%s\t%s\t%s", str1, str2, str3) !=EOF) {
+     datacount += 1;
+     if (datacount <= 10) {
+       strcpy(single_data[datacount][0],str1);
+       strcpy(single_data[datacount][1],str2);
+       strcpy(single_data[datacount][2],str3);
+       // single_data[datacount][0] = str1;
+       // single_data[datacount][1] = str2;
+       // single_data[datacount][2] = str3;
+       printf("%s,%s,%s\n",str1,str2,str3);
+       printf("datacount\n");
+       printf("%s,%s,%s\n",
+         single_data[datacount][0],
+         single_data[datacount][1],
+         single_data[datacount][2]);
+     }
+   }
 	fclose(fp); // ファイルを閉じる
 	return 0;
 }
