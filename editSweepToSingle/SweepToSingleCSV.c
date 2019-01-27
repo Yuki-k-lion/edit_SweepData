d0cc1976ac3fce112dabe41e2d0e9713f10d0a09
yuki komatsu <yuki.k.lion@gmail.com>
2019-01-27 14:32:21 +0900

    refactor:不要なコメントを削除。

--- editSweepToSingle/SweepToSingleCSV.c
+++ editSweepToSingle/SweepToSingleCSV.c
@@ -32,7 +32,6 @@ int main(void) {
      data_number = datacount%1001;
 
      if ( data_number == 1000){
-       // printf("%d\n", data_number);
        strcpy(single_data[data_number][0],str1);
        strcpy(single_data[data_number][1],str2);
        strcpy(single_data[data_number][2],str3);
@@ -43,25 +42,10 @@ int main(void) {
        strcpy(single_data[data_number][0],str1);
        strcpy(single_data[data_number][1],str2);
        strcpy(single_data[data_number][2],str3);
-       // printf("%s,%s,%s\n",str1,str2,str3);
-       // printf("datacount:%d\n",datacount);
-       // printf("\t%s\t%s\t%s\n",
-       //   single_data[data_number][0],
-       //   single_data[data_number][1],
-       //   single_data[data_number][2]);
      }
       datacount += 1;
    }
-   // printf("datacount:%d\n",datacount);
    printf("count_file:%d\n", count_file);
-  // int i;
-  // for (i = 0; i < datacount; i++) {
-  //   printf("Re:datacount:%d\n",i);
-  //   printf("debug02:\t%s\t%s\t%s\n",
-  //     single_data[i][0],
-  //     single_data[i][1],
-  //     single_data[i][2]);
-  // }
   fclose(fp); // ファイルを閉じる
 	return 0;
 }
@@ -75,14 +59,6 @@ int fileout(char data_out[ROW][COL][N]){
   //create new file
   sprintf(new_filename,"../data/single/%s.csv",data_out[0][0]);
   fpw = fopen(new_filename,"w");
-    // sprintf(new_filename,"../data/single/%s.csv",datacount);
-    //file open
-    // printf("debug02:b\t%s\t%s\t%s\n",
-    //   data_out[datacount][0],
-    //   data_out[datacount][1],
-    //   data_out[datacount][2]);
-    // printf("single_data:%s\n", data_out[datacount][0]);
-    // printf("new_filename:%s\n", new_filename);
     if (fpw == NULL) {
       printf("%s File can't open.\n",new_filename);
     } else {
