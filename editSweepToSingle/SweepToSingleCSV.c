5df6c93a57eee05f386bae738eae3119e5ee84dd
yuki komatsu <yuki.k.lion@gmail.com>
2019-01-27 12:07:45 +0900

    func:関数の切り分け

--- editSweepToSingle/SweepToSingleCSV.c
+++ editSweepToSingle/SweepToSingleCSV.c
@@ -3,16 +3,18 @@
 #include <string.h>
 #define N 256 // 1行の最大文字数(バイト数)
 #define COL 3 //ファイルデータの列数
-#define ROW 1001 //ファイルデータの行数
+#define ROW 1000 //ファイルデータの行数
+int fileout(char data_array[ROW][COL][N]); /* プロトタイプ宣言 */
 
 int main(void) {
   //宣言
-	FILE *fp,*fpw; // FILE型構造体
+	FILE *fp; // FILE型構造体
 	char file_name[] = "../data/sweep_saf_w_edit.txt";
-  char new_filename[N],single_data[ROW][COL][N];
+  char single_data[ROW][COL][N];
   // float single_data[ROW][COL];
   char str[N],str1[N],str2[N],str3[N];
   // float f1,f2,f3;
+  int file_number;
 
 	fp = fopen(file_name, "r"); // ファイルを開く。失敗するとNULLを返す。
 
@@ -26,42 +28,70 @@ int main(void) {
 
   //ファイルの中身を取り出して表示
   int datacount = 0;
-   while (fscanf(fp, "\t%s\t%s\t%s", str1, str2, str3) !=EOF) {
+   while (fscanf(fp, "\t%s\t%s\t%s", str1, str2, str3) !=EOF){
+     if (datacount%1000 == 0){
+     strcpy(single_data[1000][0],str1);
+     strcpy(single_data[1000][1],str2);
+     strcpy(single_data[1000][2],str3);
+     fileout(single_data);
 
-     if (datacount <= 10) {
-       strcpy(single_data[datacount][0],str1);
-       strcpy(single_data[datacount][1],str2);
-       strcpy(single_data[datacount][2],str3);
-       // single_data[datacount][0] = str1;
-       // single_data[datacount][1] = str2;
-       // single_data[datacount][2] = str3;
-       printf("%s,%s,%s\n",str1,str2,str3);
-       printf("datacount\n");
-       printf("\t%s\t%s\t%s\n",
-         single_data[datacount][0],
-         single_data[datacount][1],
-         single_data[datacount][2]);
+     } else {
+      file_number = datacount%1000;
+       strcpy(single_data[file_number][0],str1);
+       strcpy(single_data[file_number][1],str2);
+       strcpy(single_data[file_number][2],str3);
+         printf("%s,%s,%s\n",str1,str2,str3);
+         printf("datacount:%d\n",datacount);
+         printf("\t%s\t%s\t%s\n",
+           single_data[file_number][0],
+           single_data[file_number][1],
+           single_data[file_number][2]);
      }
-     datacount += 1;
+      datacount += 1;
    }
-	fclose(fp); // ファイルを閉じる
+   printf("datacount:%d\n",datacount);
+   // datacount = 1;
+   // printf("Re:datacount:%d\n",datacount);
+   // printf("debug01:\t%s\t%s\t%s\n",
+   //   single_data[datacount][0],
+   //   single_data[datacount][1],
+   //   single_data[datacount][2]);
+  int i;
+  for (i = 0; i < datacount; i++) {
+    printf("Re:datacount:%d\n",i);
+    printf("debug02:\t%s\t%s\t%s\n",
+      single_data[i][0],
+      single_data[i][1],
+      single_data[i][2]);
+  }
+  fclose(fp); // ファイルを閉じる
+	return 0;
+}
 
-  // printf("wright: %s\n", single_data[0][0]);
 
-  //うまくいかない
+int fileout(char data_out[ROW][COL][N]){
   //処理結果の書き込み。
+  FILE *fpw;
+  char new_filename[N];
+  int datacount;
+
   for (datacount = 0; datacount <= ROW; datacount++) {
-    sprintf(new_filename,"../data/single/%d.csv",datacount);
+    sprintf(new_filename,"../data/single/%.3s.csv",data_out[1][0]);
     //file open
-    printf("single_data:%s\n", single_data[datacount][0]);
+    printf("debug02:b\t%s\t%s\t%s\n",
+      data_out[datacount][0],
+      data_out[datacount][1],
+      data_out[datacount][2]);
+    printf("single_data:%s\n", data_out[datacount][0]);
+
     fpw = fopen(new_filename,"w");
     printf("new_filename:%s\n", new_filename);
-      if (fp == NULL) {
+      if (fpw == NULL) {
         printf("%s File can't open.\n",new_filename);
       } else {
-        fprintf(fpw, "\t%s\t%s\n", single_data[datacount][1],single_data[datacount][2]);
+        fprintf(fpw, "\t%s\t%s\n", data_out[datacount][1],data_out[datacount][2]);
       }
     }
   fclose(fpw);
-	return 0;
+  return 0;
 }
